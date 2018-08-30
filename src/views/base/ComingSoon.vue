b22b7b307cea374bb78bb6d97fe10220b20a960a
Jay Rainey <jawrainey@gmail.com>
2018-08-30 19:03:39 +0200

    :globe_with_meridians: :wrench: Added right-to-left support for Arabic
    - Remove spacing from the Arabic tagline
    - Renamed CSS classes to reflect their use
    - Removed unused functions/classes

--- src/views/base/ComingSoon.vue
+++ src/views/base/ComingSoon.vue
@@ -1,44 +1,45 @@
 <template lang="pug">
-section.pad-it
+section.side-padding
   .columns.is-centered
     .column.is-three-fifths
-      nav.level.is-mobile.margy
+      nav.level.is-mobile.add-margin
           a.level-item(
             v-for="locale in availableLocales",
             @click.prevent="setLocale(locale)",
             :class="localeClasses(locale)"
-            v-text="locale.name"
-          )
-      .columns
-        .column.has-text-centered
-          img.branding.logo(src="/static/img/logo-with-text.png", width="144px", height="144px")
-          p.strapline.is-size-5.is-size-6-mobile {{$t('comp.pre_register.tagline')}}
-      section.has-text-centered
-        a.button.is-large.is-outlined(:href="$t('comp.pre_register.action_button.url')") {{$t('comp.pre_register.action_button.text')}}
-      hr
-      .columns.is-vcentered
-        .column.is-half
-          .title.is-4.is-size-5-mobile.has-text-weight-semibold {{$t('comp.pre_register.capturing.title')}}
-          p.is-5.is-size-7-mobile {{$t('comp.pre_register.capturing.content')}}
-        .column
-          img.branding(src="/static/img/talkfutures/capturing.png")
-      hr
-      .columns.is-vcentered.reverse-on-mobile
-        .column.is-half
-          img.branding(src="/static/img/talkfutures/highlighting.png")
-        .column
-          .title.is-4.is-size-5-mobile.has-text-weight-semibold {{$t('comp.pre_register.sensemaking.title')}}
-          p.is-5.is-size-7-mobile {{$t('comp.pre_register.sensemaking.content')}}
-      hr
-      .columns.is-vcentered
-        .column.is-half
-          .title.is-4.is-size-5-mobile.has-text-weight-semibold {{$t('comp.pre_register.reuse.title')}}
-          p.is-5.is-size-7-mobile {{$t('comp.pre_register.reuse.content')}}
-        .column
-          img.branding(src="/static/img/talkfutures/sharing.png")
-      hr.no-margin-bottom
-      footer
-        nav.level.margy
+            v-text="locale.name")
+      main(:style="{ direction: setTextDirection }")
+        .columns
+          .column.has-text-centered
+            img.branding.logo(src="/static/img/logo-with-text.png")
+            p.strapline.is-size-5.is-size-6-mobile {{$t('comp.pre_register.tagline')}}
+        section.has-text-centered
+          a.button.is-large.is-outlined(:href="$t('comp.pre_register.action_button.url')") {{$t('comp.pre_register.action_button.text')}}
+        hr
+        .columns.is-vcentered
+          .column.is-half
+            .title.is-4.is-size-5-mobile.has-text-weight-semibold
+              span {{$t('comp.pre_register.capturing.title')}}
+            p.is-5.is-size-7-mobile {{$t('comp.pre_register.capturing.content')}}
+          .column
+            img.branding(src="/static/img/talkfutures/capturing.png")
+        hr
+        .columns.is-vcentered.reverse-on-mobile
+          .column.is-half
+            img.branding(src="/static/img/talkfutures/highlighting.png")
+          .column
+            .title.is-4.is-size-5-mobile.has-text-weight-semibold {{$t('comp.pre_register.sensemaking.title')}}
+            p.is-5.is-size-7-mobile {{$t('comp.pre_register.sensemaking.content')}}
+        hr
+        .columns.is-vcentered
+          .column.is-half
+            .title.is-4.is-size-5-mobile.has-text-weight-semibold {{$t('comp.pre_register.reuse.title')}}
+            p.is-5.is-size-7-mobile {{$t('comp.pre_register.reuse.content')}}
+          .column
+            img.branding(src="/static/img/talkfutures/sharing.png")
+        hr.remove-margin-bottom
+      footer.add-margin-bottom
+        nav.level.add-margin
           router-link.level-item.is-text(:to="cookiesRoute")
               | {{$t('view.base.cookies.nav_title')}}
           router-link.level-item.is-text(:to="privacyRoute")
@@ -47,14 +48,14 @@ section.pad-it
               | {{$t('view.base.research.nav_title')}}
           router-link.level-item.is-text(:to="termsRoute")
               | {{$t('view.base.terms.nav_title')}}
-        hr.no-top
+        hr.remove-top-margin
         nav.level.is-mobile
           .level-left
             .level-item
-              img.branding.bu(src="/static/img/talkfutures/ifrc.png")
+              img.bottom.branding.partners(src="/static/img/talkfutures/ifrc.png")
           .level-right
             .level-item
-              img.branding.bu.right(src="/static/img/talkfutures/solferino.png")
+              img.branding.partners.fixed-height(src="/static/img/talkfutures/solferino.png")
 </template>
 
 <script>
@@ -69,37 +70,29 @@ const availableLocales = [
 ]
 
 export default {
-  data: () => ({ availableLocales, showLocaleDropdown: false }),
+  data: () => ({ availableLocales }),
   computed: {
     privacyRoute () { return { name: PRIVACY_ROUTE } },
     termsRoute () { return { name: TERMS_ROUTE } },
     researchRoute () { return { name: RESEARCH_ROUTE } },
     cookiesRoute () { return { name: COOKIES_ROUTE } },
-    currentLocale () {
-      // TODO: If we switch to Arabic, then we must switch r-to-l
-      return availableLocales.find(l => l.key === this.$i18n.locale)
-    },
-    dropdownClasses () {
-      return { 'is-active': this.showLocaleDropdown }
-    }
+    setTextDirection () { return this.$i18n.locale === 'ar' ? 'rtl' : 'ltr' }
   },
   methods: {
-    localeClasses (locale) {
-      return { 'is-active': this.$i18n.locale === locale.key }
-    },
-    setLocale (locale) {
-      this.$i18n.locale = locale.key
-      this.showLocaleDropdown = false
-    }
+    localeClasses (locale) { return { 'is-active': this.$i18n.locale === locale.key } },
+    setLocale (locale) { this.$i18n.locale = locale.key }
   }
 }
 </script>
 
 <style lang="sass" scoped>
-.no-top
+.add-margin
+  margin: 1rem auto 0 auto
+.add-margin-bottom
+  margin-bottom: .6em
+.remove-top-margin
   margin-top: 1em
-
-.no-margin-bottom
+.remove-margin-bottom
   margin-bottom: 0
 
 .level-item
@@ -115,38 +108,9 @@ a:hover, a.is-active
 .button:hover
   border-color: #E21E26
 
-.now
-  justify-content: center
-  background: transparent
-  margin: 0 1em 1em 1em
-
-+mobile
-  .now
-    display: flex
-  .fixie .branding
-    height: 50px
-  .pad-it
-    padding: 0 1.6em
-
-  .reverse-on-mobile
-    display: flex
-    flex-direction: column-reverse
-
-+mobile
-  .bu
-    max-height: 50px !important
-    max-width: 175px !important
-  .right
-    max-height: 30px !important
-
-+tablet
-  .right
-    max-height: 40px !important
-
-.margy
-  margin: 1rem auto 0 auto
-
 .logo
+  width: 144px
+  height: 144px
   margin: 2em 0 1.25em 0 !important
 
 .strapline
@@ -156,15 +120,31 @@ a:hover, a.is-active
   max-height: 200px
   max-width: 100%
   margin: 0 auto
-.fixie > img
-  height: 80px
 
 .column
   text-align: center
 
-  .bu
-    max-height: 90px
-    max-width: 240px
-    margin: 0 auto
+.partners
+  max-height: 90px
+  max-width: 240px
+  margin: 0 auto
 
++mobile
+  .side-padding
+    padding: 0 1.6em
+
+  .reverse-on-mobile
+    display: flex
+    flex-direction: column-reverse
+
+  .partners
+    max-height: 50px !important
+    max-width: 175px !important
+
+  .fixed-height
+    max-height: 30px !important
+
++tablet
+  .fixed-height
+    max-height: 40px !important
 </style>
